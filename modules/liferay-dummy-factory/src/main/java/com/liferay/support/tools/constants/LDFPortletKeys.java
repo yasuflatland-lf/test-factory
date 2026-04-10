317b45da2bf7f898f35aae65c8231a12c4f26646
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-10 12:59:38 +0900

    Rename Java packages from com.liferay.test.factory to com.liferay.support.tools

---
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/constants/LDFPortletKeys.java
@@ -0,0 +1,8 @@
+package com.liferay.support.tools.constants;
+
+public class LDFPortletKeys {
+
+	public static final String LIFERAY_DUMMY_FACTORY =
+		"com_liferay_support_tools_portlet_LiferayDummyFactoryPortlet";
+
+}
