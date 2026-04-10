3f99fb66863f5255cc621971ef51d4ba9bd3546f
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 07:02:17 +0900

    Fix BatchNamingTest to use JUnit 5 imports

--- modules/liferay-dummy-factory/src/test/java/com/liferay/support/tools/service/BatchNamingTest.java
+++ modules/liferay-dummy-factory/src/test/java/com/liferay/support/tools/service/BatchNamingTest.java
@@ -1,33 +1,34 @@
 package com.liferay.support.tools.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BatchNamingTest {
+import org.junit.jupiter.api.Test;
+
+class BatchNamingTest {
 
 	@Test
-	public void testSingleItemUsesBaseNameDirectly() {
-		Assert.assertEquals("Site", BatchNaming.resolve("Site", 1, 0));
+	void singleItemUsesBaseNameDirectly() {
+		assertEquals("Site", BatchNaming.resolve("Site", 1, 0));
 	}
 
 	@Test
-	public void testMultipleItemsFirstIndex() {
-		Assert.assertEquals("Site1", BatchNaming.resolve("Site", 3, 0));
+	void multipleItemsFirstIndex() {
+		assertEquals("Site1", BatchNaming.resolve("Site", 3, 0));
 	}
 
 	@Test
-	public void testMultipleItemsLastIndex() {
-		Assert.assertEquals("Site3", BatchNaming.resolve("Site", 3, 2));
+	void multipleItemsLastIndex() {
+		assertEquals("Site3", BatchNaming.resolve("Site", 3, 2));
 	}
 
 	@Test
-	public void testDifferentBaseNameLastItem() {
-		Assert.assertEquals("Role5", BatchNaming.resolve("Role", 5, 4));
+	void differentBaseNameLastItem() {
+		assertEquals("Role5", BatchNaming.resolve("Role", 5, 4));
 	}
 
 	@Test
-	public void testSeparatorVariant() {
-		Assert.assertEquals("Org 1", BatchNaming.resolve("Org", 3, 0, " "));
+	void separatorVariant() {
+		assertEquals("Org 1", BatchNaming.resolve("Org", 3, 0, " "));
 	}
 
 }
