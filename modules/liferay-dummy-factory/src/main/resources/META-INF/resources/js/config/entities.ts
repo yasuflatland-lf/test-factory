a7f9564ed834c6a57288c00fa5ac066f1cf58be8
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-10 21:20:56 +0900

    Add User entity configuration and language keys

--- modules/liferay-dummy-factory/src/main/resources/META-INF/resources/js/config/entities.ts
+++ modules/liferay-dummy-factory/src/main/resources/META-INF/resources/js/config/entities.ts
@@ -42,8 +42,94 @@ const ORGANIZATION_CONFIG: EntityFormConfig = {
 	label: 'organizations',
 };
 
+const USER_CONFIG: EntityFormConfig = {
+	actionURL: '/ldf/user',
+	entityType: ENTITY_TYPES.USERS,
+	fields: [
+		{
+			label: 'number-of-users',
+			name: 'count',
+			required: true,
+			type: 'number',
+			validators: [
+				{message: 'please-enter-a-valid-number', type: 'digits'},
+				{message: 'value-must-be-greater-than-0', type: 'min', value: 1},
+			],
+		},
+		{
+			label: 'base-user-name',
+			name: 'baseName',
+			required: true,
+			type: 'text',
+		},
+		{
+			advanced: true,
+			defaultValue: 'liferay.com',
+			label: 'email-domain',
+			name: 'emailDomain',
+			required: false,
+			type: 'text',
+		},
+		{
+			advanced: true,
+			defaultValue: 'test',
+			label: 'password',
+			name: 'password',
+			required: false,
+			type: 'text',
+		},
+		{
+			advanced: true,
+			defaultValue: true,
+			label: 'male',
+			name: 'male',
+			required: false,
+			type: 'toggle',
+		},
+		{
+			advanced: true,
+			defaultValue: '',
+			label: 'job-title',
+			name: 'jobTitle',
+			required: false,
+			type: 'text',
+		},
+		{
+			advanced: true,
+			dataSource: '/ldf/data/organizations',
+			defaultValue: '',
+			label: 'organizations',
+			name: 'organizationIds',
+			required: false,
+			type: 'multiselect',
+		},
+		{
+			advanced: true,
+			dataSource: '/ldf/data/roles',
+			defaultValue: '',
+			label: 'roles',
+			name: 'roleIds',
+			required: false,
+			type: 'multiselect',
+		},
+		{
+			advanced: true,
+			dataSource: '/ldf/data/user-groups',
+			defaultValue: '',
+			label: 'user-groups',
+			name: 'userGroupIds',
+			required: false,
+			type: 'multiselect',
+		},
+	],
+	helpText: 'user-help-text',
+	icon: 'user',
+	label: 'users',
+};
+
 export const ENTITY_CONFIGS: Partial<Record<EntityType, EntityFormConfig>> = {
 	[ENTITY_TYPES.ORGANIZATION]: ORGANIZATION_CONFIG,
+	[ENTITY_TYPES.USERS]: USER_CONFIG,
 };
 
 export function getEntityConfig(entityType: EntityType): EntityFormConfig | undefined {
