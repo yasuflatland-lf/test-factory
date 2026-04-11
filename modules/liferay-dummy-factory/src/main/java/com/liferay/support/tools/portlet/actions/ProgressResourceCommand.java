21acfc1b1b9d3323c7c23ca1c3e2d839a508620d
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 12:20:15 +0900

    Add ProgressResourceCommand polling endpoint for progress tracker

---
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/portlet/actions/ProgressResourceCommand.java
@@ -0,0 +1,100 @@
+package com.liferay.support.tools.portlet.actions;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.ProgressTracker;
+import com.liferay.support.tools.constants.LDFPortletKeys;
+
+import javax.portlet.PortletSession;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Serves the current progress percentage for a long-running creator job.
+ *
+ * <p>
+ * The creator services drive a {@link ProgressTracker} via
+ * {@code ProgressManager}. That tracker installs itself into the
+ * {@link PortletSession} under the key
+ * {@link ProgressTracker#PERCENT}+{@code progressId}. This endpoint reads the
+ * tracker back out of the session and returns its current percent as JSON:
+ * </p>
+ *
+ * <pre>{@code
+ * {"percent": 42}
+ * }</pre>
+ *
+ * @author Yasuyuki Takeo
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + LDFPortletKeys.LIFERAY_DUMMY_FACTORY,
+		"mvc.command.name=/ldf/progress"
+	},
+	service = MVCResourceCommand.class
+)
+public class ProgressResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		JSONObject responseJson = JSONFactoryUtil.createJSONObject();
+
+		try {
+			String progressId = ParamUtil.getString(
+				resourceRequest, _PROGRESS_ID_PARAM, _DEFAULT_PROGRESS_ID);
+
+			int percent = _readPercent(resourceRequest, progressId);
+
+			responseJson.put("percent", percent);
+		}
+		catch (Throwable throwable) {
+			_log.error("Failed to read progress", throwable);
+
+			responseJson.put("percent", 0);
+			responseJson.put("error", throwable.getMessage());
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse, responseJson);
+	}
+
+	private int _readPercent(
+		ResourceRequest resourceRequest, String progressId) {
+
+		PortletSession portletSession = resourceRequest.getPortletSession(
+			false);
+
+		if (portletSession == null) {
+			return 0;
+		}
+
+		Object attribute = portletSession.getAttribute(
+			ProgressTracker.PERCENT + progressId,
+			PortletSession.APPLICATION_SCOPE);
+
+		if (attribute instanceof ProgressTracker) {
+			return ((ProgressTracker)attribute).getPercent();
+		}
+
+		return 0;
+	}
+
+	private static final String _DEFAULT_PROGRESS_ID = "COMMON_PROGRESS_ID";
+
+	private static final String _PROGRESS_ID_PARAM = "COMMON_PROGRESS_ID";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ProgressResourceCommand.class);
+
+}
