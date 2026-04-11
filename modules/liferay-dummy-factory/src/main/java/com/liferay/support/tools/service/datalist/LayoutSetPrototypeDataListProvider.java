b3ab30f27717356ce85d779d568418c46e9f39db
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 15:08:15 +0900

    fix QueryUtil import in LayoutSetPrototypeDataListProvider

--- modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/service/datalist/LayoutSetPrototypeDataListProvider.java
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/service/datalist/LayoutSetPrototypeDataListProvider.java
@@ -1,5 +1,6 @@
 package com.liferay.support.tools.service.datalist;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -8,7 +9,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.QueryUtil;
 import com.liferay.support.tools.service.DataListProvider;
 
 import java.util.List;
