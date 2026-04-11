a128f6b109e5e79bed80b34d5503002a6eb1fbc8
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 18:20:02 +0900

    add per-iteration error handling and skipped tracking to LayoutCreator

--- modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/service/LayoutCreator.java
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/service/LayoutCreator.java
@@ -1,9 +1,13 @@
 package com.liferay.support.tools.service;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.LayoutFriendlyURLException;
+import com.liferay.portal.kernel.exception.LayoutNameException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.service.LayoutLocalService;
@@ -22,42 +26,88 @@ public class LayoutCreator {
 	public JSONObject create(
 			long userId, BatchSpec batchSpec, long groupId, String type,
 			boolean privateLayout, boolean hidden)
-		throws Throwable {
+		throws Exception {
 
 		int count = batchSpec.count();
 		String baseName = batchSpec.baseName();
 
 		JSONObject result = JSONFactoryUtil.createJSONObject();
 		JSONArray layouts = JSONFactoryUtil.createJSONArray();
+		int skipped = 0;
 
 		for (int i = 0; i < count; i++) {
 			final String name = BatchNaming.resolve(baseName, count, i, " ");
 
-			Layout layout = TransactionInvokerUtil.invoke(
+			try {
+				Layout layout = _invokeAddLayout(
+					userId, groupId, privateLayout, name, type, hidden);
+
+				JSONObject layoutJson = JSONFactoryUtil.createJSONObject();
+
+				layoutJson.put("friendlyURL", layout.getFriendlyURL());
+				layoutJson.put("layoutId", layout.getLayoutId());
+				layoutJson.put(
+					"name", layout.getName(LocaleUtil.getSiteDefault()));
+				layoutJson.put("plid", layout.getPlid());
+
+				layouts.put(layoutJson);
+			}
+			catch (LayoutFriendlyURLException | LayoutNameException e) {
+				_log.warn(
+					"Layout '" + name + "' could not be created: " +
+						e.getMessage(),
+					e);
+
+				skipped++;
+			}
+		}
+
+		int createdCount = layouts.length();
+
+		result.put("count", createdCount);
+		result.put("layouts", layouts);
+		result.put("skipped", skipped);
+		result.put("success", createdCount > 0);
+
+		if (createdCount == 0) {
+			result.put(
+				"error",
+				"No layouts were created (all names may be invalid or " +
+					"already exist)");
+		}
+
+		if (skipped > 0) {
+			result.put(
+				"message",
+				skipped + " layout(s) were skipped");
+		}
+
+		return result;
+	}
+
+	private Layout _invokeAddLayout(
+			long userId, long groupId, boolean privateLayout, String name,
+			String type, boolean hidden)
+		throws Exception {
+
+		try {
+			return TransactionInvokerUtil.invoke(
 				_transactionConfig,
 				() -> _layoutLocalService.addLayout(
 					StringPool.BLANK, userId, groupId, privateLayout,
 					LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, name,
 					StringPool.BLANK, StringPool.BLANK, type, hidden,
 					StringPool.BLANK, new ServiceContext()));
-
-			JSONObject layoutJson = JSONFactoryUtil.createJSONObject();
-
-			layoutJson.put("friendlyURL", layout.getFriendlyURL());
-			layoutJson.put("layoutId", layout.getLayoutId());
-			layoutJson.put(
-				"name", layout.getName(LocaleUtil.getSiteDefault()));
-			layoutJson.put("plid", layout.getPlid());
-
-			layouts.put(layoutJson);
 		}
-
-		result.put("count", count);
-		result.put("layouts", layouts);
-		result.put("success", true);
-
-		return result;
+		catch (Exception e) {
+			throw e;
+		}
+		catch (Throwable t) {
+			throw new Exception(t);
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(LayoutCreator.class);
 
 	private static final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
