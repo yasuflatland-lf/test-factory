9473731b8303e44ec638aecef8f7e059bef5ecf7
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 12:12:56 +0900

    Add Journal/DDM/crawler4j dependencies and WCM portlet constants

--- modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/constants/LDFPortletKeys.java
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/constants/LDFPortletKeys.java
@@ -5,4 +5,10 @@ public class LDFPortletKeys {
 	public static final String LIFERAY_DUMMY_FACTORY =
 		"com_liferay_support_tools_portlet_LiferayDummyFactoryPortlet";
 
+	public static final String WEB_CONTENT = "webcontent";
+
+	public static final String DDM_STRUCTURE_KEY = "BASIC-WEB-CONTENT";
+
+	public static final String EOL = "\n";
+
 }
