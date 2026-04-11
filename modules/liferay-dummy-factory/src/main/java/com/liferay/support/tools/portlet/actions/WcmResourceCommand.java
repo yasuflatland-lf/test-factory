e009c3d807fad14e1fc025e131e3b46c333d01f7
Yasuyuki Takeo <yasuflatland@gmail.com>
2026-04-11 12:20:09 +0900

    Add WcmResourceCommand dispatching to WebContentCreator by mode

---
+++ modules/liferay-dummy-factory/src/main/java/com/liferay/support/tools/portlet/actions/WcmResourceCommand.java
@@ -0,0 +1,181 @@
+package com.liferay.support.tools.portlet.actions;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.support.tools.constants.LDFPortletKeys;
+import com.liferay.support.tools.service.BatchSpec;
+import com.liferay.support.tools.service.WebContentCreator;
+
+import java.util.Locale;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	property = {
+		"javax.portlet.name=" + LDFPortletKeys.LIFERAY_DUMMY_FACTORY,
+		"mvc.command.name=/ldf/wcm"
+	},
+	service = MVCResourceCommand.class
+)
+public class WcmResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		HttpServletRequest httpServletRequest =
+			_portal.getOriginalServletRequest(
+				_portal.getHttpServletRequest(resourceRequest));
+
+		String dataString = ParamUtil.getString(
+			httpServletRequest, "data");
+
+		JSONObject responseJson = JSONFactoryUtil.createJSONObject();
+
+		try {
+			JSONObject data = JSONFactoryUtil.createJSONObject(dataString);
+
+			BatchSpec batchSpec = ResourceCommandUtil.parseBatchSpec(data);
+
+			long groupId = GetterUtil.getLong(data.getString("groupId"));
+
+			if (groupId <= 0) {
+				throw new IllegalArgumentException(
+					"groupId must be a positive number");
+			}
+
+			long folderId = GetterUtil.getLong(data.getString("folderId"));
+			String[] locales = _parseLocales(
+				data.getString("locales"), groupId);
+			boolean neverExpire = GetterUtil.getBoolean(
+				data.getString("neverExpire"), true);
+			boolean neverReview = GetterUtil.getBoolean(
+				data.getString("neverReview"), true);
+
+			int createContentsType = GetterUtil.getInteger(
+				data.getString("createContentsType"));
+
+			long userId = _portal.getUserId(resourceRequest);
+
+			int count = batchSpec.count();
+			String baseName = batchSpec.baseName();
+
+			if (createContentsType == 0) {
+				String baseArticle = GetterUtil.getString(
+					data.getString("baseArticle"));
+
+				responseJson = _webContentCreator.createSimple(
+					userId, groupId, count, baseName, baseArticle, folderId,
+					locales, neverExpire, neverReview);
+			}
+			else if (createContentsType == 1) {
+				int titleWords = GetterUtil.getInteger(
+					data.getString("titleWords"), 5);
+				int totalParagraphs = GetterUtil.getInteger(
+					data.getString("totalParagraphs"), 3);
+				int randomAmount = GetterUtil.getInteger(
+					data.getString("randomAmount"), 3);
+				String linkLists = GetterUtil.getString(
+					data.getString("linkLists"));
+
+				responseJson = _webContentCreator.createDummy(
+					userId, groupId, count, baseName, folderId, locales,
+					titleWords, totalParagraphs, randomAmount, linkLists,
+					neverExpire, neverReview);
+			}
+			else if (createContentsType == 2) {
+				long ddmStructureId = GetterUtil.getLong(
+					data.getString("ddmStructureId"));
+				long ddmTemplateId = GetterUtil.getLong(
+					data.getString("ddmTemplateId"));
+
+				if (ddmStructureId <= 0) {
+					throw new IllegalArgumentException(
+						"ddmStructureId must be a positive number");
+				}
+
+				if (ddmTemplateId <= 0) {
+					throw new IllegalArgumentException(
+						"ddmTemplateId must be a positive number");
+				}
+
+				responseJson = _webContentCreator.createWithStructureTemplate(
+					userId, groupId, count, baseName, folderId, locales,
+					ddmStructureId, ddmTemplateId, neverExpire, neverReview);
+			}
+			else {
+				throw new IllegalArgumentException(
+					"Unknown createContentsType: " + createContentsType);
+			}
+		}
+		catch (IllegalArgumentException illegalArgumentException) {
+			ResourceCommandUtil.setErrorResponse(
+				responseJson, illegalArgumentException);
+		}
+		catch (Throwable throwable) {
+			_log.error("Failed to create web contents", throwable);
+
+			ResourceCommandUtil.setErrorResponse(responseJson, throwable);
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse, responseJson);
+	}
+
+	private String[] _parseLocales(String localesCsv, long groupId) {
+		if (Validator.isNotNull(localesCsv)) {
+			String[] tokens = localesCsv.split(",");
+			int nonEmpty = 0;
+
+			for (String token : tokens) {
+				if ((token != null) && !token.trim().isEmpty()) {
+					nonEmpty++;
+				}
+			}
+
+			if (nonEmpty > 0) {
+				String[] result = new String[nonEmpty];
+				int idx = 0;
+
+				for (String token : tokens) {
+					if ((token != null) && !token.trim().isEmpty()) {
+						result[idx++] = token.trim();
+					}
+				}
+
+				return result;
+			}
+		}
+
+		Locale defaultLocale = _portal.getSiteDefaultLocale(groupId);
+
+		return new String[] {LocaleUtil.toLanguageId(defaultLocale)};
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WcmResourceCommand.class);
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private WebContentCreator _webContentCreator;
+
+}
